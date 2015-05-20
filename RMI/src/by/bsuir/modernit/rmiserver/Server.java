d763bbf77d9c92ddac5f505f80c515b97068e1c0
Daniel Gorgeous <daniel.serko@gmail.com>
2015-05-20 17:07:26 +0300

    RMI

---
+++ RMI/src/by/bsuir/modernit/rmiserver/Server.java
@@ -0,0 +1,34 @@
+package by.bsuir.modernit.rmiserver;
+
+import by.bsuir.modernit.core.Hello;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server implements Hello {
+
+    public Server() {}
+
+    @Override
+    public String sayHello() {
+        return "Hello, world!";
+    }
+
+    public static void main(String args[]) {
+
+        try {
+            Server obj = new Server();
+            Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.createRegistry(12345);
+            registry.bind("Hello", stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ RMIClient/src/by/bsuir/modernit/rmiclient/Client.java
@@ -0,0 +1,24 @@
+package by.bsuir.modernit.rmiclient;
+
+
+import by.bsuir.modernit.core.Hello;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+
+    private Client() {}
+
+    public static void main(String[] args) {
+
+        try {
+            Registry registry = LocateRegistry.getRegistry(null, 12345);
+            Hello stub = (Hello) registry.lookup("Hello");
+            String response = stub.sayHello();
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
